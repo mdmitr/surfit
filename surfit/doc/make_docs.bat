6de8645135516ad6f7bcbda86793383818c1e2b0
mishadm <mishadm@3bdad9f7-c964-442a-868d-553b97b55ca0>
2006-04-06 04:22:44 +0000

    don't call htmlhelp
    git-svn-id: svn://localhost/trunk@714 3bdad9f7-c964-442a-868d-553b97b55ca0

--- surfit/doc/make_docs.bat
+++ surfit/doc/make_docs.bat
@@ -10,9 +10,6 @@ del formula.*
 del _form*.*
 cd ..
 
-rem copy ..\surfit\doc\surfit.css surfit.css
-rem copy ..\surfit\doc\surfit_head.html surfit_head.html
-rem copy ..\surfit\doc\surfit_foot.html surfit_foot.html
 copy gallery\*.* html\*.*
 copy gallery\data\*.* html\*.*
 copy gallery\maps\*.* html\*.*
@@ -23,9 +20,6 @@ rem cd html
 rem "C:\Program Files\HTML Help Workshop\hhc.exe" index.hhp
 rem cd ..
 rem copy html\surfit.chm ..\bin\surfit.chm
-
-rem perl installdox -lsurfit_src.tag@../../surfit/surfit_src/html
-rem del *.bak
 
 copy src\index.html html\index.html
 
