ba058a9160b633830c2f6b4c287f048c44f75b49
mishadm <mishadm@3bdad9f7-c964-442a-868d-553b97b55ca0>
2006-04-10 10:24:22 +0000

    some scripts added
    git-svn-id: svn://localhost/trunk@721 3bdad9f7-c964-442a-868d-553b97b55ca0

--- surfit/examples/surfit/run_all.tcl
+++ surfit/examples/surfit/run_all.tcl
@@ -3,59 +3,80 @@ load libsurfit[info sharedlibextension]
 #
 # curvs 
 #
-cd curvs
-if { [source area.tcl] == 0 } {
+cd "curvs"
+if { [source "area.tcl"] == 0 } {
 	return 0
 }
-if { [source area_ineq.tcl] == 0 } {
+if { [source "area_ineq.tcl"] == 0 } {
 	return 0
 }
-if { [source area_mean.tcl] == 0 } {
+if { [source "area_mean.tcl"] == 0 } {
 	return 0
 }
-if { [source area_wmean.tcl] == 0 } {
+if { [source "area_surf.tcl"] == 0 } {
 	return 0
 }
-if { [source contour.tcl] == 0 } {
+if { [source "area_surf_ineq.tcl"] == 0 } {
 	return 0
 }
-if { [source curve.tcl] == 0 } {
+if { [source "area_wmean.tcl"] == 0 } {
 	return 0
 }
-if { [source fault.tcl] == 0 } {
+if { [source "contour.tcl"] == 0 } {
+	return 0
+}
+if { [source "contour_ineq.tcl"] == 0 } {
+	return 0
+}
+if { [source "curve.tcl"] == 0 } {
+	return 0
+}
+if { [source "curve_ineq.tcl"] == 0 } {
+	return 0
+}
+if { [source "curve_surf.tcl"] == 0 } {
+	return 0
+}
+if { [source "curve_surf_ineq.tcl"] == 0 } {
+	return 0
+}
+if { [source "fault.tcl"] == 0 } {
 	return 0
 }
 cd ..
 
 #
-# funcs
+# surfaces
 #
-cd funcs
-if { [source func.tcl] == 0 } {
+cd "surfs"
+if { [source "mask.tcl"] == 0 } {
 	return 0
 }
-if { [source func_ineq.tcl] == 0 } {
+if { [source "surface.tcl"] == 0 } {
 	return 0
 }
-if { [source mask.tcl] == 0 } {
+if { [source "surface_add.tcl"] == 0 } {
 	return 0
 }
-if { [source trend.tcl] == 0 } {
+if { [source "surface_ineq.tcl"] == 0 } {
+	return 0
+}
+if { [source "trend.tcl"] == 0 } {
 	return 0
 }
 cd ..
 
 #
-# global
+# others
 #
-cd global
-if { [source ineq.tcl] == 0 } {
+cd "others"
+if { [source "ineq.tcl"] == 0 } {
 	return 0
 }
-if { [source mean.tcl] == 0 } {
+if { [source "mean.tcl"] == 0 } {
 	return 0
 }
-if { [source wmean.tcl] == 0 } {
+if { [source "wmean.tcl"] == 0 } {
 	return 0
 }
 cd ..
@@ -63,14 +84,14 @@ cd ..
 #
 # points
 #
-cd points
-if { [source points_approx.tcl] == 0 } {
+cd "points"
+if { [source "points_approx.tcl"] == 0 } {
 	return 0
 }
-if { [source points_exact.tcl] == 0 } {
+if { [source "points_exact.tcl"] == 0 } {
 	return 0
 }
-if { [source points_ineq.tcl] == 0 } {
+if { [source "points_ineq.tcl"] == 0 } {
 	return 0
 }
 cd ..
