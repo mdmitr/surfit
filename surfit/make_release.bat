54b9e7650218aba6dde00e51b81adea1f1949411
mishadm <mishadm@3bdad9f7-c964-442a-868d-553b97b55ca0>
2006-07-12 11:10:45 +0000

    libsurfit_io
    git-svn-id: svn://localhost/trunk@825 3bdad9f7-c964-442a-868d-553b97b55ca0

--- surfit/make_release.bat
+++ surfit/make_release.bat
@@ -1,8 +1,9 @@
 mkdir bin
 cd vc6
 del *.dep
-nmake.exe sstuff.mak 	CFG="sstuff - Win32 Release"
-nmake.exe surfit.mak 	CFG="surfit - Win32 Release"
-nmake.exe freeflow.mak 	CFG="freeflow - Win32 Release"
-nmake.exe globe.mak 	CFG="globe - Win32 Release"
+nmake.exe sstuff.mak 		CFG="sstuff - Win32 Release"
+nmake.exe surfit.mak 		CFG="surfit - Win32 Release"
+nmake.exe freeflow.mak 		CFG="freeflow - Win32 Release"
+nmake.exe globe.mak 		CFG="globe - Win32 Release"
+nmake.exe surfit_io.mak 	CFG="surfit_io - Win32 Release"
 cd ..
