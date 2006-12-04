cd0bc559cea86a5724a1249791872294571d029f
mvd <mvd@3bdad9f7-c964-442a-868d-553b97b55ca0>
2006-12-04 18:14:40 +0000

    Removed file/folder
    git-svn-id: svn://localhost/trunk@890 3bdad9f7-c964-442a-868d-553b97b55ca0

--- make_debug.bat
+++
@@ -1,8 +0,0 @@
-mkdir bin
-cd vc6
-del *.dep
-nmake.exe sstuff.mak 	CFG="sstuff - Win32 Debug"
-nmake.exe surfit.mak 	CFG="surfit - Win32 Debug"
-nmake.exe freeflow.mak 	CFG="freeflow - Win32 Debug"
-nmake.exe globe.mak 	CFG="globe - Win32 Debug"
-cd ..
