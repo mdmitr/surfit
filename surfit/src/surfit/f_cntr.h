c7f649ac319c2556abce6efda989720cf0846a72
mishadm <mishadm@3bdad9f7-c964-442a-868d-553b97b55ca0>
2007-06-16 04:17:22 +0000

    f_cntr2 was removed
    git-svn-id: svn://localhost/trunk@1109 3bdad9f7-c964-442a-868d-553b97b55ca0

--- surfit/src/surfit/f_cntr.h
+++ surfit/src/surfit/f_cntr.h
@@ -23,16 +23,7 @@
 #include "functional.h"
 #include "f_points.h"
 
-#include <set>
-
 namespace surfit {
-
-struct cell_record
-{
-	cell_record() : sum_value(0), cnt(0) {};
-	REAL sum_value;
-	size_t cnt;
-};
 
 class d_points;
 class f_points;
@@ -60,65 +51,6 @@ protected:
 
 	//! contour for approximation
 	const d_cntr * contour;
-};
-
-struct sect
-{
-	//! distance to neighbour cell
-	REAL d; 
-	
-	//! desired value
-	REAL z; 
-	
-	//! lower cell number
-	size_t pos1;
-
-	//! higher cell number
-	size_t pos2;
-
-};
-
-class SURFIT_EXPORT f_cntr2 : public functional {
-public:
-	//! constructor
-	f_cntr2(bool iflag, REAL imult = 1);
-	//! destructor
-	~f_cntr2();
-
-	const char * getManagerName() const { return "surfit"; };
-
-	bool minimize();
-	bool make_matrix_and_vector(matr *& matrix, extvec *& v, bitvec * mask_solved, bitvec * mask_undefined);
-	void mark_solved_and_undefined(bitvec * mask_solved, 
-				       bitvec * mask_undefined,
-				       bool i_am_cond);
-	
-	bool solvable_without_cond(const bitvec * mask_solved, 
-				   const bitvec * mask_undefined,
-				   const extvec * X);
-
-	virtual void cleanup();
-
-	void add_contour(const d_cntr * cntr);
-	bool get_flag() const;
-	
-protected:
-
-	int this_get_data_count() const;
-	const data * this_get_data(int pos) const;
-
-	//! contour for approximation
-	std::vector<const d_cntr *> * contours;
-
-private:
-	void calc_sects(bitvec * mask_solved, bitvec * mask_undefined);
-	d_grid * sects_grid;
-	std::vector<sect> * sects;
-	REAL mult;
-	bool flag;
-
-	std::set<size_t, cell_record> recs;
-
 };
 
 }; // namespace surfit
