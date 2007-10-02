61b7ca05d415876b79916506af636362645f54e1
mishadm <mishadm@3bdad9f7-c964-442a-868d-553b97b55ca0>
2007-10-02 04:26:34 +0000

    error output for surfit_cmd
    git-svn-id: svn://localhost/trunk@1212 3bdad9f7-c964-442a-868d-553b97b55ca0

--- surfit/src/surfit_cmd/surfit_cmd.cpp
+++ surfit/src/surfit_cmd/surfit_cmd.cpp
@@ -1,34 +1,72 @@
 
 #include <tcl.h>
+#include <string.h>
 
 #ifdef _MSC_VER
 #pragma warning(disable : 4786)
 #pragma warning(disable : 4996)
 #endif
 
-int AppInit(Tcl_Interp *interp) {
+int AppInit(Tcl_Interp *interp) 
+{
 	if(Tcl_Init(interp) == TCL_ERROR) 
 		return TCL_ERROR;
+
+	char * new_string = "\n";
+	Tcl_Channel out = NULL;
+	if (interp) {
+		out = Tcl_GetStdChannel(TCL_STDOUT);
+	}
+
 	Tcl_Eval(interp, "info sharedlibextension");
 	const char * res = Tcl_GetStringResult(interp);
 	char buf[256];
 	sprintf(buf, "load libsurfit%s", res);
-	Tcl_Eval(interp, buf);
+	int result = Tcl_Eval(interp, buf);
+	if (result != TCL_OK) {
+		res = Tcl_GetStringResult(interp);
+		if (out) {
+			Tcl_WriteChars(out, res, (int)strlen(res));
+			Tcl_WriteChars(out, new_string, 1);
+		}
+	}
 	
 	Tcl_Eval(interp, "info sharedlibextension");
 	res = Tcl_GetStringResult(interp);
 	sprintf(buf, "load libsurfit_io%s", res);
-	Tcl_Eval(interp, buf);
+	result = Tcl_Eval(interp, buf);
+	if (result != TCL_OK) {
+		res = Tcl_GetStringResult(interp);
+		if (out) {
+			Tcl_WriteChars(out, res, (int)strlen(res));
+			Tcl_WriteChars(out, new_string, 1);
+		}
+	}
 	
 	Tcl_Eval(interp, "info sharedlibextension");
 	res = Tcl_GetStringResult(interp);
 	sprintf(buf, "load libglobe%s", res);
-	Tcl_Eval(interp, buf);
+	result = Tcl_Eval(interp, buf);
+	if (result != TCL_OK) {
+		res = Tcl_GetStringResult(interp);
+		if (out) {
+			Tcl_WriteChars(out, res, (int)strlen(res));
+			Tcl_WriteChars(out, new_string, 1);
+		}
+	}
 
 	Tcl_Eval(interp, "info sharedlibextension");
 	res = Tcl_GetStringResult(interp);
 	sprintf(buf, "load libfreeflow%s", res);
-	Tcl_Eval(interp, buf);
+	result = Tcl_Eval(interp, buf);
+	if (result != TCL_OK) {
+		res = Tcl_GetStringResult(interp);
+		if (out) {
+			Tcl_WriteChars(out, res, (int)strlen(res));
+			Tcl_WriteChars(out, new_string, 1);
+		}
+	}
+
 	return TCL_OK;
 }
 
