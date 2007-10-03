c4534afdb96a0a38ad96b9bc76b36bd6c82bea6f
mishadm <mishadm@3bdad9f7-c964-442a-868d-553b97b55ca0>
2007-10-03 02:51:36 +0000

    error output for surfit_cmd 2
    git-svn-id: svn://localhost/trunk@1214 3bdad9f7-c964-442a-868d-553b97b55ca0

--- surfit/src/surfit_cmd/surfit_cmd.cpp
+++ surfit/src/surfit_cmd/surfit_cmd.cpp
@@ -13,10 +13,13 @@ int AppInit(Tcl_Interp *interp)
 		return TCL_ERROR;
 
 	char * new_string = "\n";
+	char * message_on_error = "\n\nSome errors detected while starting surfit!!!\nPlease refer \"Getting started\" documentation page.\n\n";
 	Tcl_Channel out = NULL;
 	if (interp) {
 		out = Tcl_GetStdChannel(TCL_STDOUT);
 	}
+
+	bool errors_on_loading = false;
 
 	Tcl_Eval(interp, "info sharedlibextension");
 	const char * res = Tcl_GetStringResult(interp);
@@ -24,6 +27,7 @@ int AppInit(Tcl_Interp *interp)
 	sprintf(buf, "load libsurfit%s", res);
 	int result = Tcl_Eval(interp, buf);
 	if (result != TCL_OK) {
+		errors_on_loading = true;
 		res = Tcl_GetStringResult(interp);
 		if (out) {
 			Tcl_WriteChars(out, res, (int)strlen(res));
@@ -36,6 +40,7 @@ int AppInit(Tcl_Interp *interp)
 	sprintf(buf, "load libsurfit_io%s", res);
 	result = Tcl_Eval(interp, buf);
 	if (result != TCL_OK) {
+		errors_on_loading = true;
 		res = Tcl_GetStringResult(interp);
 		if (out) {
 			Tcl_WriteChars(out, res, (int)strlen(res));
@@ -48,6 +53,7 @@ int AppInit(Tcl_Interp *interp)
 	sprintf(buf, "load libglobe%s", res);
 	result = Tcl_Eval(interp, buf);
 	if (result != TCL_OK) {
+		errors_on_loading = true;
 		res = Tcl_GetStringResult(interp);
 		if (out) {
 			Tcl_WriteChars(out, res, (int)strlen(res));
@@ -60,11 +66,17 @@ int AppInit(Tcl_Interp *interp)
 	sprintf(buf, "load libfreeflow%s", res);
 	result = Tcl_Eval(interp, buf);
 	if (result != TCL_OK) {
+		errors_on_loading = true;
 		res = Tcl_GetStringResult(interp);
 		if (out) {
 			Tcl_WriteChars(out, res, (int)strlen(res));
 			Tcl_WriteChars(out, new_string, 1);
 		}
+	}
+
+	if (errors_on_loading)
+	{
+		Tcl_WriteChars(out, message_on_error, (int)strlen(message_on_error));
 	}
 
 	return TCL_OK;
