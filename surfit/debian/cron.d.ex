04656e56d2e68721f393222ae84a68fcc39c5bb1
mishadm <mishadm@3bdad9f7-c964-442a-868d-553b97b55ca0>
2007-12-04 18:41:52 +0000

    debian packaging rules
    git-svn-id: svn://localhost/trunk@1239 3bdad9f7-c964-442a-868d-553b97b55ca0

---
+++ surfit/debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the surfit package
+#
+0 4	* * *	root	surfit_maintenance
---
+++ surfit/debian/surfit-default.ex
@@ -0,0 +1,10 @@
+# Defaults for surfit initscript
+# sourced by /etc/init.d/surfit
+# installed at /etc/default/surfit by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
