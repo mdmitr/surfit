2f5b7b8cab44aa82274942789ec390d6b762ce4b
mdmitr <mdmitr@gmail.com>
2013-11-19 10:11:13 +0600

    seismic example

---
+++ surfit/examples/surfit/realworld/seis/00_run_all.tcl
@@ -0,0 +1,7 @@
+source 01_2d_seism.tcl
+source 02_3d_seism.tcl
+source 03_top_tvd.tcl
+source 04_base_tvd.tcl
+source 05_top_tvd_simple.tcl
+
+
--- surfit/examples/surfit/realworld/seis/run_all.tcl
+++
@@ -1,7 +0,0 @@
-source 2d_seism.tcl
-source 3d_seism.tcl
-source aoup.tcl
-source aobt.tcl
-source aoup_simple.tcl
-
-
