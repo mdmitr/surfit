a46be23f4dd762d89c167e4f0bb777de8428db76
Mikhail Dmitrievsky <mdmitr@gmail.com>
2015-10-22 10:35:09 +0500

    building with Visual Studio 2015 and python 3.5

--- make_sln.cmd
+++ make_sln.cmd
@@ -3,4 +3,4 @@ echo off
 mkdir build
 cd build
 
-cmake ..\src\ 
+cmake ..\src\ -G "Visual Studio 14 2015" -DSWIG_EXECUTABLE=c:\swig\swig.exe -DSWIG_DIR=c:\swig\ -DPYTHON_INCLUDE_DIR=C:\Python35\include\ -DPYTHON_LIBRARY=C:\Python35\libs\python35.lib 
